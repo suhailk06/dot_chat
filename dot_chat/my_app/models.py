5e3d5de2acc07885b4dc03f469ce63992ff06bf1
suhailk06 <suhailkanamukkala@gmail.com>
2026-08-01 22:32:38 +0530

    updated register and login page

--- dot_chat/my_app/models.py
+++ dot_chat/my_app/models.py
@@ -6,7 +6,7 @@ class UserData(models.Model):
     email = models.EmailField(unique=True)
     username = models.CharField(max_length=50, unique=True)
     password = models.CharField(max_length=128)
-    profile_pic = models.CharField(max_length=255, default='default.jpg')
+    profile_pic = models.CharField(max_length=255, default='default.png')
     bio = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     
