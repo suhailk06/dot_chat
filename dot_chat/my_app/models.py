fbd243bddbeb1a43e8db3c3f4597456153294c0d
suhailk06 <suhailkanamukkala@gmail.com>
2026-08-07 22:04:15 +0530

    added new background and new logo

--- dot_chat/my_app/models.py
+++ dot_chat/my_app/models.py
@@ -6,7 +6,7 @@ class UserData(models.Model):
     email = models.EmailField(unique=True)
     username = models.CharField(max_length=50, unique=True)
     password = models.CharField(max_length=128)
-    profile_pic = models.CharField(max_length=255, default='default.png')
+    profile_pic = models.CharField(max_length=255, default='default_pic.png')
     bio = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     is_verified = models.BooleanField(default=False)
